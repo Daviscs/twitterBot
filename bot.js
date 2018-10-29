ead45ceea22bc5743c9f545f7f254d0569fdeaa4
Daviscs <austinjoey@ymail.com>
2018-10-28 23:33:08 -0400

    Adding twitter bot

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
-//Like an import statement for node
 var Twit = require('twit');
 var config = require('./config');
-
 var T = new Twit(config);
 
-//---------------Twitting---------------------
-function tweetIt(txt){
+//Posts a status
+function tweetIt(text){
+
+//Message for the status
     var tweet = {
-      status: txt
+      status: text
     }
 
     T.post('statuses/update', tweet, tweeted)
@@ -16,28 +16,32 @@ function tweetIt(txt){
     }
 }
 
-//Message when account tweets about off-white shoe
+//Monitors followers
 var streamer = T.stream('user', 'KicksDeals');
 streamer.on('tweet', tweetAlert);
 
 function tweetAlert(eventMsg){
   var message = eventMsg.text;
   var name = eventMsg.user.screen_name;
+
+//Words that the bot is looking for in the status
   var offWhiteMsg = message.includes("SNKS");
   var offWhiteMsg2 = message.includes("Off-White")
 
+//Checks if the status contain the specific word
   if(offWhiteMsg == true || offWhiteMsg2 == true){
     tweet = {
-      screen_name: 'daveyTrann',
-      text: 'Hello @' + name +' posted something about off-white!!'
+     //The user that the bot will notify
+      screen_name: "Bobby",
+      text: 'Hello @' + name +' posted about Bobby!!'
     }
 
+//Notifys the user by private message that a status contains the specfic word
     T.post("direct_messages/new", tweet)
   }
-
 }
 
-//-------------If someone follows bot--------------
+//Runs when a user follows the bot
 var streamer = T.stream("user");
 streamer.on('follow', sendingMsg);
 
@@ -46,10 +50,13 @@ var name = eventMsg.source.screen_name,
 
 tweet = {
   screen_name: name,
-  text: 'Hello ' + name + ' thanks for the follow and contributing for the lols'
+  text: 'Hello ' + name + ' thanks for the follow!'
 }
 
-tweetIt('@' + name + ' thanks for the follow. Oh you are also beautiful, teehee :)');
+//The status message when someone follows bot
+tweetIt('@' + name + ' thanks for the follow! You are also beautiful :)');
+
+//Sends a private message to the user
 T.post("direct_messages/new", tweet)
 
 }
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
+//Add the bots config
 module.exports = {
-  consumer_key:         'KaRf5kom2emxbeSyN5wm7CvP3',
-  consumer_secret:      'ny2kAZ4jybJjMrg5uWACZWFR0BV05IrRTeGYZjTOYx4jCbuYSG',
-  access_token:         '1000151643302825985-YPM7fAt0vfAAWeM4xrRX8zFNw7ID8y',
-  access_token_secret:  'FSrOPS0ulKHVtxB48ESHmNampC6KeLmCcJjgb3TtTNVeF',
-
+  consumer_key:         ,
+  consumer_secret:      ,
+  access_token:         ,
+  access_token_secret:  ,
 }
